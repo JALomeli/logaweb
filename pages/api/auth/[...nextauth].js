6eece8d0fbf8e9e8aef70b319df202da1e7c56cb
Lomeli <josea.lomeligar.scch@gmail.com>
2023-12-11 20:53:55 -0600

    Add GoogleProvider to authentication options

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github"
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import clientPromise from "@/lib/mongodb"
@@ -9,7 +10,10 @@ export const authOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-    }),
+    }), GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    })
     // ...add more providers here
   ],
   adapter: MongoDBAdapter(clientPromise),
